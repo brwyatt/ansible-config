1bfad940d451e11ed43bbde1d9998d820ecb5393
Bryan Wyatt <brwyatt@gmail.com>
2025-12-22 19:54:59 +0000

    Add check_cert function

--- roles/ipacert/library/cert.py
+++ roles/ipacert/library/cert.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
-from typing import Optional, Set, TypedDict
+from typing import Optional, TypedDict
 import re
 import subprocess
 
@@ -14,6 +14,63 @@ kerberos_principal_regex = re.compile(
     r'(?P<instance>[^@]+)@'
     r'(?P<realm>[\w\.]+))\s*$'
 )
+
+class CertData(TypedDict):
+    cert_name: str
+    present: bool
+    status: Optional[str]
+    stuck: Optional[bool]
+    key_path: Optional[str]
+    cert_path: Optional[str]
+    subject: Optional[str]
+    domain: Optional[str]
+    track: Optional[bool]
+    auto_renew: Optional[bool]
+
+
+def check_cert(cert_name: str) -> CertData:
+    cmd = ['ipa-getcert', 'list', '--id', cert_name]
+    try:
+        result = subprocess.run(cmd, capture_output=True, text=True, check=False)
+    except FileNotFoundError as e:
+        raise RuntimeError(f"ipa-getcert failed: {e}")
+    if (
+        result.returncode == 1 and
+        "No request found with specified nickname" in result.stdout
+    ):
+        return {
+            'cert_name': cert_name,
+            'present': False,
+            "status": None,
+            "stuck": None,
+            "key_path": None,
+            "cert_path": None,
+            "subject": None,
+            "domain": None,
+            "track": None,
+            "auto_renew": None,
+        }
+    cert_name_match = re.search(r'^Request ID \'(.*)\':$', result.stdout, re.MULTILINE)
+    status_match = re.search(r'^\s*status: (.+)$', result.stdout, re.MULTILINE)
+    stuck_match = re.search(r'^\s*stuck: (yes|no)$', result.stdout, re.MULTILINE)
+    key_match = re.search(r'^\s*key pair storage: type=FILE,location=\'(.*)\'$', result.stdout, re.MULTILINE)
+    cert_match = re.search(r'^\s*certificate: type=FILE,location=\'(.*)\'$', result.stdout, re.MULTILINE)
+    subject_match = re.search(r'^\s*subject: (.+)$', result.stdout, re.MULTILINE)
+    domain_match = re.search(r'^\s*dns: (.+)$', result.stdout, re.MULTILINE)
+    track_match = re.search(r'^\s*track: (yes|no)$', result.stdout, re.MULTILINE)
+    auto_renew_match = re.search(r'^\s*auto-renew: (yes|no)$', result.stdout, re.MULTILINE)
+    return {
+        'cert_name': cert_name_match.group(1),
+        'present': True,
+        'status': status_match.group(1),
+        'stuck': stuck_match.group(1) == "yes",
+        'key_path': key_match.group(1),
+        'cert_path': cert_match.group(1),
+        'subject': subject_match.group(1),
+        'domain': domain_match.group(1),
+        'track': track_match.group(1) == "yes",
+        'auto_renew': auto_renew_match.group(1) == "yes",
+    }
 
 
 def get_host_principal():
@@ -34,6 +91,8 @@ def main():
             'cert_name': {'required': False, 'type': 'str'},
             'domain': {'required': False, 'type': 'str'},
             'principal': {'required': False, 'type': 'str'},
+            'cert_path': {'required': True, 'type': 'str'},
+            'key_path': {'required': True, 'type': 'str'},
             'state': {'choices': ['present', 'absent'], 'default': 'present', 'type': 'str'},
         },
         supports_check_mode=True
@@ -44,6 +103,8 @@ def main():
     principal = module.params['principal']
     if principal is None or principal == "":
         principal = get_host_principal()
+    cert_path = module.params['cert_path']
+    key_path = module.params['key_path']
 
 
 if __name__ == '__main__':
